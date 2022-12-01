1ba772232f28401f9d3bc9287bdb9f62cf24e2c9
Daniele <daniele.cmp24@gmail.com>
2022-12-01 23:43:39 +0100

    add .gitignore

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,4 @@
+fun main(args: Array<String>) {
+    println("The Elf carrying more calories is carrying ${getCaloriesTopNElves(1)} calories")
+    println("The top 3 Elves carrying more calories are carrying ${getCaloriesTopNElves(3)} calories")
+}
---
+++ src/main/kotlin/first.kt
@@ -0,0 +1,9 @@
+fun getCaloriesTopNElves(n : Int) : Int {
+    val content = getResourceAsText("inputday1.txt")
+    val strings : List<String> = content?.replace("\n\n", " ")?.split(" ") ?: emptyList()
+    val listBags : List<List<Int>> = strings.map { it.split("\n").map { n -> n.toInt() } }.toList()
+    return listBags.sortedBy { it.sum() }.reversed().take(n).flatten().sum()
+}
+
+fun getResourceAsText(path: String): String? =
+    object {}.javaClass.getResource(path)?.readText()
